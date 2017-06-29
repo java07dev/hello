6f459a15f5e94313ab8c31ae4527b9b2d1ba0ff3
xizho10 <josh_chow@163.com>
2017-06-29 20:13:33 +0800

    first commit
    Signed-off-by: xizho10 <josh_chow@163.com>

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("===hello=zhengjie==");
+        System.out.println("===hello=zhengjie==\n\n");
+        //发送 GET 请求
+        String s=HttpRequest.sendGet("http://139.196.115.69:20334/api/v1/node/connectioncount", "key=123&v=456");
+        System.out.println(s);
+
+        //发送 POST 请求
+       String sr=HttpRequest.sendPost("http://139.196.115.69:20334/api/v1/transaction?access_token=b73e1d242991c7a32ba77b7e19c7140f", "key=123&v=456");
+        System.out.println(sr);
     }
 }
